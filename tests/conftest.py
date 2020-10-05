d9a3a303028b615dc4ea9d874682ad7b207c3f88
Aidan <aidanbfitzgerald@gmail.com>
2020-10-05 18:43:47 +1300

    added user to repo fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 import os
 from flix.adapters import memory_repository
 from flix.adapters.memory_repository import MemoryRepository, populate
+from flix.domain.model import User
 
 
 @pytest.fixture
@@ -10,4 +11,5 @@ def in_memory_repo():
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'data')
     populate(filename, repo)
+    repo.add_user(User("shaun", '12345'))
     return repo
