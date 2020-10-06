17801525e2a2691bc243977f29c31ddaa5f47436
Aidan <aidanbfitzgerald@gmail.com>
2020-10-07 02:05:09 +1300

    registered home and movies blueprints

--- flix/__init__.py
+++ flix/__init__.py
@@ -8,7 +8,18 @@ from flix.adapters.memory_repository import MemoryRepository, populate
 
 def create_app():
     app = Flask(__name__)
+
     app.config.from_object('config.Config')
     data_path = os.path.join('flix', 'adapters', 'data')
+
     repo.repo_instance = MemoryRepository()
     populate(data_path, repo.repo_instance)
+
+    with app.app_context():
+        from .home import home
+        app.register_blueprint(home.home_blueprint)
+
+        from .movies import movies
+        app.register_blueprint(movies.movies_blueprint)
+
+    return app
