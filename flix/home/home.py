fc0afa990693a715ab5f070f689ccc13e86c0cf6
Aidan <aidanbfitzgerald@gmail.com>
2020-10-11 03:19:42 +1300

    added watchlist to home page

--- flix/home/home.py
+++ flix/home/home.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
+from flix.adapters import repository as repo
+from flix.movies import services
 
 home_blueprint = Blueprint('home_bp', __name__)
 
 
 @home_blueprint.route('/', methods=['GET'])
 def home():
-    return render_template("home/home.html")
+    watchlist = services.get_watchlist(repo.repo_instance)
+    return render_template("home/home.html", watchlist=watchlist)
