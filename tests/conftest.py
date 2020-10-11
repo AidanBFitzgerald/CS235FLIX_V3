703297a48dbd8d19c879bd311db5df7732813f87
Aidan <aidanbfitzgerald@gmail.com>
2020-10-11 18:30:08 +1300

    added client fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,14 @@
 import pytest
 import os
-from flix.adapters import memory_repository
+
+from werkzeug.security import generate_password_hash
+
+from flix import create_app
+import flix.adapters.repository as repo1
 from flix.adapters.memory_repository import MemoryRepository, populate
 from flix.domain.model import User
+
+TEST_DATA_PATH = os.path.join('tests', 'data')
 
 
 @pytest.fixture
@@ -13,3 +19,31 @@ def in_memory_repo():
     populate(filename, repo)
     repo.add_user(User("shaun", '12345'))
     return repo
+
+
+@pytest.fixture
+def client():
+    my_app = create_app({'TESTING': True,  # Set to True during testing.
+                         'TEST_DATA_PATH': TEST_DATA_PATH,  # Path for loading test data into the repository.
+                         'WTF_CSRF_ENABLED': False})
+    repo1.repo_instance.add_user(User("shaun", generate_password_hash("12345")))
+    return my_app.test_client()
+
+
+class AuthenticationManager:
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='shaun', password='12345'):
+        return self._client.post(
+            'authentication/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthenticationManager(client)
