0a6a32b3e7354ff0723d412c627a7206b105d5d1
Aidan <aidanbfitzgerald@gmail.com>
2020-10-11 18:30:24 +1300

    added test data path

--- flix/__init__.py
+++ flix/__init__.py
@@ -6,11 +6,16 @@ import flix.adapters.repository as repo
 from flix.adapters.memory_repository import MemoryRepository, populate
 
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config.from_object('config.Config')
     data_path = os.path.join('flix', 'adapters', 'data')
+
+    if test_config is not None:
+        # Load test configuration, and override any configuration settings.
+        app.config.from_mapping(test_config)
+        data_path = app.config['TEST_DATA_PATH']
 
     repo.repo_instance = MemoryRepository()
     populate(data_path, repo.repo_instance)
