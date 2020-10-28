320878c78e34ec4dfe046edd4f39f01f41e25f3a
Aidan <aidanbfitzgerald@gmail.com>
2020-10-29 02:53:17 +1300

    added database fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,22 @@
 import pytest
 import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import clear_mappers, sessionmaker
 from werkzeug.security import generate_password_hash
 
 from flix import create_app
 import flix.adapters.repository as repo1
+from flix.adapters import database_repository
 from flix.adapters.memory_repository import MemoryRepository, populate
+from flix.adapters.orm import metadata, map_model_to_tables
 from flix.domain.model import User
 
-TEST_DATA_PATH = os.path.join('tests', 'data')
+TEST_DATA_PATH_MEMORY = os.path.join('tests', 'data')
+TEST_DATA_PATH_DATABASE = os.path.join('tests', 'data', 'database')
+
+TEST_DATABASE_URI_IN_MEMORY = 'sqlite://'
+TEST_DATABASE_URI_FILE = 'sqlite:///CS235FLIX-test.db'
 
 
 @pytest.fixture
@@ -19,6 +27,63 @@ def in_memory_repo():
     populate(filename, repo)
     repo.add_user(User("shaun", '12345'))
     return repo
+
+
+@pytest.fixture
+def database_engine():
+    engine = create_engine(TEST_DATABASE_URI_FILE)
+    clear_mappers()
+    metadata.create_all(engine)  # Conditionally create database tables.
+    for table in reversed(metadata.sorted_tables):  # Remove any data from the tables.
+        engine.execute(table.delete())
+    map_model_to_tables()
+    database_repository.populate(engine, TEST_DATA_PATH_DATABASE)
+    yield engine
+    metadata.drop_all(engine)
+    clear_mappers()
+
+
+@pytest.fixture
+def empty_session():
+    engine = create_engine(TEST_DATABASE_URI_IN_MEMORY)
+    metadata.create_all(engine)
+    for table in reversed(metadata.sorted_tables):
+        engine.execute(table.delete())
+    map_model_to_tables()
+    session_factory = sessionmaker(bind=engine)
+    yield session_factory()
+    metadata.drop_all(engine)
+    clear_mappers()
+
+
+@pytest.fixture
+def session():
+    clear_mappers()
+    engine = create_engine(TEST_DATABASE_URI_IN_MEMORY)
+    metadata.create_all(engine)
+    for table in reversed(metadata.sorted_tables):
+        engine.execute(table.delete())
+    map_model_to_tables()
+    session_factory = sessionmaker(bind=engine)
+    database_repository.populate(engine, TEST_DATA_PATH_DATABASE)
+    yield session_factory()
+    metadata.drop_all(engine)
+    clear_mappers()
+
+
+@pytest.fixture
+def session_factory():
+    clear_mappers()
+    engine = create_engine(TEST_DATABASE_URI_IN_MEMORY)
+    metadata.create_all(engine)
+    for table in reversed(metadata.sorted_tables):
+        engine.execute(table.delete())
+    map_model_to_tables()
+    session_factory = sessionmaker(bind=engine)
+    database_repository.populate(engine, TEST_DATA_PATH_DATABASE)
+    yield session_factory
+    metadata.drop_all(engine)
+    clear_mappers()
 
 
 @pytest.fixture
