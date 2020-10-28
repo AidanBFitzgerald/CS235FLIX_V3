30f29d615d0141266c45f727bbbee4a48574bb3d
Aidan <aidanbfitzgerald@gmail.com>
2020-10-28 16:03:48 +1300

    added initialization of database repo

--- flix/__init__.py
+++ flix/__init__.py
@@ -1,6 +1,8 @@
 import os
 
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
 import flix.adapters.repository as repo
 from flix.adapters.memory_repository import MemoryRepository, populate
@@ -17,8 +19,40 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
         data_path = app.config['TEST_DATA_PATH']
 
-    repo.repo_instance = MemoryRepository()
-    populate(data_path, repo.repo_instance)
+    if app.config['REPOSITORY'] == 'memory':
+        # Create the MemoryRepository instance for a memory-based repository.
+        repo.repo_instance = MemoryRepository()
+        populate(data_path, repo.repo_instance)
+
+    elif app.config['REPOSITORY'] == 'database':
+        # Configure database for database repo
+        database_uri = app.config["SQLALCHEMY_DATABASE_URI"]
+
+        database_echo = app.config['SQLALCHEMY_ECHO']
+        database_engine = create_engine(database_uri, connect_args={"check_same_thread": False}, poolclass=NullPool,
+                                        echo=database_echo)
+
+        if app.config['TESTING'] == 'True' or len(database_engine.table_names()) == 0:
+            print("REPOPULATING DATABASE")
+            # For testing, or first-time use of the web application, reinitialise the database.
+            clear_mappers()
+            metadata.create_all(database_engine)  # Conditionally create database tables.
+            for table in reversed(metadata.sorted_tables):  # Remove any data from the tables.
+                database_engine.execute(table.delete())
+
+            # Generate mappings that map domain model classes to the database tables.
+            map_model_to_tables()
+
+            database_repository.populate(database_engine, data_path)
+
+        else:
+            # Solely generate mappings that map domain model classes to the database tables.
+            map_model_to_tables()
+
+            # Create the database session factory using sessionmaker (this has to be done once, in a global manner)
+        session_factory = sessionmaker(autocommit=False, autoflush=True, bind=database_engine)
+        # Create the SQLAlchemy DatabaseRepository instance for an sqlite3-based repository.
+        repo.repo_instance = database_repository.SqlAlchemyRepository(session_factory)
 
     with app.app_context():
         from .home import home
@@ -29,5 +63,17 @@ def create_app(test_config=None):
 
         from .authentication import authentication
         app.register_blueprint(authentication.authentication_blueprint)
+
+        # Reset the session inside the database repository before a new flask request is generated
+        @app.before_request
+        def before_flask_http_request_function():
+            if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
+                repo.repo_instance.reset_session()
+
+        # Register a tear-down method that will be called after each request has been processed.
+        @app.teardown_appcontext
+        def shutdown_session(exception=None):
+            if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
+                repo.repo_instance.close_session()
 
     return app
