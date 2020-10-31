e4015acb7735554e69015d53991b9565a641f913
Aidan <aidanbfitzgerald@gmail.com>
2020-10-31 17:25:35 +1300

    added database

--- flix/__init__.py
+++ flix/__init__.py
@@ -1,18 +1,25 @@
+"""Initialize Flask app."""
+
 import os
 
 from flask import Flask
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import clear_mappers, sessionmaker
+from sqlalchemy.orm import sessionmaker, clear_mappers
 from sqlalchemy.pool import NullPool
 
 import flix.adapters.repository as repo
-from flix.adapters import database_repository, memory_repository
+from flix.adapters import memory_repository, database_repository
 from flix.adapters.orm import metadata, map_model_to_tables
 
 
 def create_app(test_config=None):
+    """Construct the core application."""
+
+    # Create the Flask app object.
     app = Flask(__name__)
 
+    # Configure the app from configuration-file settings.
     app.config.from_object('config.Config')
     data_path = os.path.join('flix', 'adapters', 'data')
 
@@ -21,15 +28,22 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
         data_path = app.config['TEST_DATA_PATH']
 
+    # Here the "magic" of our repository pattern happens. We can easily switch between in memory data and
+    # persistent database data storage for our application.
+
     if app.config['REPOSITORY'] == 'memory':
         # Create the MemoryRepository instance for a memory-based repository.
         repo.repo_instance = memory_repository.MemoryRepository()
         memory_repository.populate(data_path, repo.repo_instance)
 
     elif app.config['REPOSITORY'] == 'database':
-        # Configure database for database repo
-        database_uri = app.config["SQLALCHEMY_DATABASE_URI"]
+        # Configure database.
+        database_uri = app.config['SQLALCHEMY_DATABASE_URI']
 
+        # We create a comparatively simple SQLite database, which is based on a single file (see .env for URI).
+        # For example the file database could be located locally and relative to the application in covid-19.db,
+        # leading to a URI of "sqlite:///covid-19.db".
+        # Note that create_engine does not establish any actual DB connection directly!
         database_echo = app.config['SQLALCHEMY_ECHO']
         database_engine = create_engine(database_uri, connect_args={"check_same_thread": False}, poolclass=NullPool,
                                         echo=database_echo)
@@ -51,12 +65,15 @@ def create_app(test_config=None):
             # Solely generate mappings that map domain model classes to the database tables.
             map_model_to_tables()
 
-            # Create the database session factory using sessionmaker (this has to be done once, in a global manner)
+        # Create the database session factory using sessionmaker (this has to be done once, in a global manner)
         session_factory = sessionmaker(autocommit=False, autoflush=True, bind=database_engine)
         # Create the SQLAlchemy DatabaseRepository instance for an sqlite3-based repository.
         repo.repo_instance = database_repository.SqlAlchemyRepository(session_factory)
 
+
+    # Build the application - these steps require an application context.
     with app.app_context():
+
         from .home import home
         app.register_blueprint(home.home_blueprint)
 
@@ -66,7 +83,8 @@ def create_app(test_config=None):
         from .authentication import authentication
         app.register_blueprint(authentication.authentication_blueprint)
 
-        # Reset the session inside the database repository before a new flask request is generated
+        # Register a callback the makes sure that database sessions are associated with http requests
+        # We reset the session inside the database repository before a new flask request is generated
         @app.before_request
         def before_flask_http_request_function():
             if isinstance(repo.repo_instance, database_repository.SqlAlchemyRepository):
